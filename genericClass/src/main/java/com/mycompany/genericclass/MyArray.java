9407b5e403daeb44bf03b5c6f6b5ef7b1e34f44d
lalolipc <mardelplatarocio@gmail.com>
2020-03-25 19:15:23 -0300

    punto 1 y 2 de la guia

---
+++ genericClass/src/main/java/com/mycompany/genericclass/MyArray.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.genericclass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Lec222
+ */
+public class MyArray<T extends Comparable> {
+      List<T> list = null;
+    public MyArray() {
+    list=new ArrayList<T>();   
+    }
+    public void add(T objeto){
+    list.add(objeto);
+    }
+
+    @Override
+    public String toString() {
+        return "MyArray{" + "list=" + list + '}';
+    }
+
+    public boolean exist(T myElement){
+        return list.contains(myElement);
+    }
+
+    public T greater(){
+        T myElement=list.get(0);
+        T GreaterElement = null;
+        int r;
+      for(int x = 0;x<list.size();x++)
+      {
+            r= list.get(x).compareTo(myElement);
+                 if(r>0){
+                     GreaterElement=list.get(x);
+                }else
+                     GreaterElement=myElement;
+            }
+       
+    return GreaterElement;
+    }
+    
+     public T less(){
+        T myElement=list.get(0);
+        T GreaterElement = null;
+        int r;
+      for(int x = 0;x<list.size();x++)
+      {
+            r= list.get(x).compareTo(myElement);
+                 if(r<0){
+                     GreaterElement=list.get(x);
+                }else
+                     GreaterElement=myElement;
+            }
+       
+    return GreaterElement;
+    }
+  
+   public T deleteLast(){
+       return  list.remove(list.size()-1);
+   }
+    
+}
